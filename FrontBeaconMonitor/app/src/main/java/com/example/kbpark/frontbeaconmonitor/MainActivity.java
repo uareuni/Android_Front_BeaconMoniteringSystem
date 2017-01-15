a515d3328363287b02b7690af9cded56614a7936
uareuni <uareuni@gmail.com>
2017-01-15 16:42:56 +0900

    Login UI - 1

--- FrontBeaconMonitor/app/src/main/java/com/example/kbpark/frontbeaconmonitor/MainActivity.java
+++ FrontBeaconMonitor/app/src/main/java/com/example/kbpark/frontbeaconmonitor/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.kbpark.frontbeaconmonitor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passEditText;
+
+    private Button resister_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init()
+    {
+        // Address the email and password field
+        emailEditText = (EditText) findViewById(R.id.username);
+        passEditText = (EditText) findViewById(R.id.password);
+
+        resister_btn = (Button) findViewById(R.id.resister_btn);
+        resister_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class); // to SignUp
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void checkLogin(View arg0)
+    {
+
+        final String email = emailEditText.getText().toString();
+        if (!isValidEmail(email)) {
+            //Set error message for email field
+            emailEditText.setError("Invalid Email");
+        }
+
+        final String pass = passEditText.getText().toString();
+        if (!isValidPassword(pass)) {
+            //Set error message for password field
+            passEditText.setError("Password cannot be empty");
+        }
+
+        if(isValidEmail(email) && isValidPassword(pass))
+        {
+            // Validation Completed
+        }
+
+    }
+
+
+    /**
+     * -----------------  < optional part >  ------------------------
+     */
+
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() >= 4) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ FrontBeaconMonitor/app/src/main/java/com/example/kbpark/frontbeaconmonitor/SignUp.java
@@ -0,0 +1,19 @@
+package com.example.kbpark.frontbeaconmonitor;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KBPark on 2017. 1. 15..
+ */
+
+public class SignUp extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        setContentView(R.layout.signup);
+        super.onCreate(savedInstanceState);
+    }
+}
